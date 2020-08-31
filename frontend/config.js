d3fa5e09f80de193987e171845b86844beae406c
Igor Kazakevich <igor.kazakevich79@gmail.com>
2020-08-31 22:49:45 +0300

    online store

---
+++ frontend/config.js
@@ -0,0 +1,4 @@
+// This is client side config only - don't put anything in here that shouldn't be public!
+export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://sick-fits-b8e1fcd65a.herokuapp.com/`;
+export const perPage = 4;
